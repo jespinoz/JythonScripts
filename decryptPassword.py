374a16cd20113114977e9e51d419b8d828e7dde4
Jose Espinoza Cruz <jose.espinoza.c@gmail.com>
2016-05-27 12:54:30 -0300

    Update decryptPassword.py
    Python script to decrypt a WebLogic password

--- decryptPassword.py
+++ decryptPassword.py
@@ -2,9 +2,7 @@ from weblogic.security.internal import *
 from weblogic.security.internal.encryption import *
 
 #This will prompt you to make sure you have SerializedSystemIni.dat file under #current directory from where you are running command
-
 raw_input("Please make sure you have SerializedSystemIni.dat inside the current directory, if yes press ENTER to continue.")
-
 
 # Encryption service
 encryptionService = SerializedSystemIni.getEncryptionService(".")
@@ -13,10 +11,8 @@ clearOrEncryptService = ClearOrEncryptedService(encryptionService)
 # Take encrypt password from user
 pwd = raw_input("Please enter encrypted password (Eg. {3DES}Bxt5E3...): ")
 
-
 # Delete unnecessary escape characters
 preppwd = pwd.replace("\\", "")
-
 
 # Decrypt password
 print "Your password is: " + clearOrEncryptService.decrypt(preppwd)
